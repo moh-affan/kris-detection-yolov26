2df2374320d326368a2721879c32aa869cf8d5af
Affan <b4affan@gmail.com>
2026-06-09 19:28:45 +0700

    add allowed host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig(({ mode }) => {
     server: {
       port: 3000,
       host: '0.0.0.0',
+      allowedHosts: ['localhost', 'keris.affan-ai.my.id'],
       proxy: {
         '/api': {
           target: backendTarget,
